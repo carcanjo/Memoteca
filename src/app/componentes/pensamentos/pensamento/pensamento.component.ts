816ed91b54bc9a210574c03e0360689c60074c73
Cleiton Arcanjo <60898489+carcanjo@users.noreply.github.com>
2023-08-25 19:09:03 -0300

    Implementação do favoritos

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -12,9 +13,10 @@ export class PensamentoComponent implements OnInit {
     id: 1,
     conteudo: 'I Love Angular',
     autoria: 'Cleiton',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
-  constructor() { }
+  constructor( private service: PensamentoService) { }
 
   ngOnInit() {
   }
@@ -25,5 +27,18 @@ export class PensamentoComponent implements OnInit {
     }
 
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string{
+    if (this.pensamento.favorito) {
+      return 'ativo'
+    }
+
+    return 'inativo'
+  }
+
+  atualizarFavorito() {
+    this.service.mudarFavorito(this.pensamento)
+      .subscribe();
   }
 }
